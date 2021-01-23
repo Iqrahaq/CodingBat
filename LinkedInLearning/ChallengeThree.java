cae54d8d714fd37dd220af0bcf649bfa5f5a2173
Iqra <iqra.haq@northumbria.ac.uk>
2021-01-23 16:22:41 +0000

    LinkedIn Learning

---
+++ LinkedInLearning/ChallengeThree.java
@@ -0,0 +1,22 @@
+public class ChallengeThree {
+
+    public static double calculateEmployeeSalary(double hoursPerWeek, double ratePerHour, double vacationDays){
+
+        if (hoursPerWeek < 0) {
+            return -1;
+        }
+        if (ratePerHour < 0){
+            return -1;
+        }
+
+        double vacationRate = vacationDays * ratePerHour * 8;
+        double ratePerWeek = ratePerHour * hoursPerWeek;
+        double ratePerYear = (ratePerWeek * 52) - vacationRate;
+        return ratePerYear;
+    }
+
+    public static void main(String[] args) {
+        double employeeSalary = calculateEmployeeSalary(40, 15, 8);
+        System.out.println(employeeSalary);
+    }
+}
